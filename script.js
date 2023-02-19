51da1095db079101445c8354a2ebba319ff267e8
Juan Carrillo <juan@juanca.org>
2023-02-19 13:25:14 -0600

    feat(script): agrega logica del modal y backdrop

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hey! 👋");
+const modal = document.querySelector(".main__modal");
+const backdrop = document.querySelector(".main__backdrop");
+const packagesButton = document.querySelector(".main__cta--packages");
+
+function openModal() {
+  modal.classList.remove("hidden");
+  backdrop.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  backdrop.classList.add("hidden");
+}
+
+packagesButton.addEventListener("click", openModal);
+backdrop.addEventListener("click", closeModal);
